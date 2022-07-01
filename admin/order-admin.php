b3be1f06883b39a10f93f9c38207f912ce8bfaf1
haikalbakhtiar <79645465+haikalbakhtiar@users.noreply.github.com>
2022-07-02 01:09:20 +0800

    nothing happen

--- admin/order-admin.php
+++ admin/order-admin.php
@@ -39,16 +39,16 @@ include "../config/danger_notification.php";
                 <!--==================================================-->
                 <!--                  Items                           -->
                 <!--==================================================-->
-                <div class="col-lg-9 col-sm-12">
+                <div class="col-lg-12">
                     <!-- ============================================-->
                     <!-- <section> begin ============================-->
                     <div class="row text-center">
                         <div class="col">
                             <div class="container">
                                 <div class="row">
-                                    <div class="col-md-6 mt-2">
+                                    <div class="col-md-4 mt-2">
                                         <div class="card card-span h-100">
-                                            <a id="badgeBtn" class="btn stretched-link btn-info" onclick="updateCard(1)">
+                                            <a id="upcomingNewOrderbtn" class="btn stretched-link btn-info" onclick="updateCard(1)">
                                                 <div class="card-body pt-4 pb-4">
                                                     <h5>Upcoming New Order</h5>
                                                     <p>5</p>
@@ -57,9 +57,9 @@ include "../config/danger_notification.php";
                                             </a>
                                         </div>
                                     </div>
-                                    <div class="col-md-6 mt-2">
+                                    <div class="col-md-4 mt-2">
                                         <div class="card card-span h-100">
-                                            <a id="stickerBtn" class="btn stretched-link" onclick="updateCard(2)">
+                                            <a id="processingbtn" class="btn stretched-link" onclick="updateCard(2)">
                                                 <div class="card-body pt-4 pb-4">
                                                     <h5>Processing</h5>
                                                     <p>20</p>
@@ -67,9 +67,9 @@ include "../config/danger_notification.php";
                                             </a>
                                         </div>
                                     </div>
-                                    <div class="col-md-6 mt-2">
+                                    <div class="col-md-4 mt-2">
                                         <div class="card card-span h-100">
-                                            <a id="stickerBtn" class="btn stretched-link" onclick="updateCard(2)">
+                                            <a id="completedOrderbtn" class="btn stretched-link" onclick="updateCard(3)">
                                                 <div class="card-body pt-4 pb-4">
                                                     <h5>Completed Order</h5>
                                                     <p>999</p>
@@ -80,19 +80,19 @@ include "../config/danger_notification.php";
                                 </div>
                             </div> <!-- end of .container-->
 
-  <div class="container-fluid mt-7">
+  <div class="container-fluid mt-6">
     <div class="row">
-      <div class="col-12">
+      <div class="col-lg-12">
         <div class="card">
-          <div class="card-body">Order List
-              <div id="tableExample3" data-list='{"valueNames":["name","email","payment"],"filter":true}'>
+            <div class="card-body"> <h1>Order List</h1>
+              <div id="orderListTable" data-list='{"valueNames":["UniqueID","InvoiceNumber","OrderStatus","DatePlaced","CustomerID"],"filter":true}'>
                   <div class="row justify-content-end g-0">
                       <div class="col-auto px-3">
                           <select class="form-select form-select-sm mb-3" aria-label="Bulk actions" data-list-filter="data-list-filter">
                               <option selected="" value="">Select payment status</option>
-                              <option value="Pending">Pending</option>
-                              <option value="Success">Success</option>
-                              <option value="Blocked">Blocked</option>
+                              <option value="Processing">Processing</option>
+                              <option value="Completed Order">Completed Order</option>
+                              <option value="Upcoming New Order">Upcoming New Order</option>
                           </select>
                       </div>
                   </div>
@@ -100,70 +100,72 @@ include "../config/danger_notification.php";
                       <table class="table table-sm table-striped fs--1 mb-0 overflow-hidden">
                           <thead class="bg-200 text-900">
                           <tr>
-                              <th class="sort pe-1 align-middle white-space-nowrap" data-sort="name">Customer</th>
-                              <th class="sort pe-1 align-middle white-space-nowrap" data-sort="email">Email</th>
-                              <th class="sort align-middle white-space-nowrap text-end pe-4" data-sort="payment">Payment</th>
+                              <th class="sort pe-1 align-middle white-space-nowrap" data-sort="UniqueID">UniqueID</th>
+                              <th class="sort pe-1 align-middle white-space-nowrap" data-sort="InvoiceNumber">InvoiceNumber</th>
+                              <th class="sort align-middle white-space-nowrap text-end pe-4" data-sort="OrderStatus<">OrderStatus</th>
+                              <th class="sort pe-1 align-middle white-space-nowrap" data-sort="DatePlaced">DatePlaced</th>
+                              <th class="sort pe-1 align-middle white-space-nowrap" data-sort="CustomerID">CustomerID</th>
                           </tr>
                           </thead>
                           <tbody class="list" id="table-purchase-body">
                           <tr class="btn-reveal-trigger">
-                              <th class="align-middle white-space-nowrap name"><a href="../../app/e-commerce/customer-details.html">Sylvia Plath</a></th>
-                              <td class="align-middle white-space-nowrap email">john@gmail.com</td>
-                              <td class="align-middle text-end fs-0 white-space-nowrap payment"> <span class="badge badge rounded-pill badge-soft-success">Success<span class="ms-1 fas fa-check" data-fa-transform="shrink-2"></span></span>
+                              <th class="align-middle white-space-nowrap UniqueID"><a href="../../app/e-commerce/customer-details.html">Sylvia Plath</a></th>
+                              <td class="align-middle white-space-nowrap InvoiceNumber">john@gmail.com</td>
+                              <td class="align-middle text-end fs-0 white-space-nowrap OrderStatus"> <span class="badge badge rounded-pill badge-soft-success">Completed Order<span class="ms-1 fas fa-check" data-fa-transform="shrink-2"></span></span>
                               </td>
                           </tr>
                           <tr class="btn-reveal-trigger">
-                              <th class="align-middle white-space-nowrap name"><a href="../../app/e-commerce/customer-details.html">Homer</a></th>
-                              <td class="align-middle white-space-nowrap email">sylvia@mail.ru</td>
-                              <td class="align-middle text-end fs-0 white-space-nowrap payment"> <span class="badge badge rounded-pill badge-soft-warning">Pending<span class="ms-1 fas fa-stream" data-fa-transform="shrink-2"></span></span>
+                              <th class="align-middle white-space-nowrap UniqueID"><a href="../../app/e-commerce/customer-details.html">Homer</a></th>
+                              <td class="align-middle white-space-nowrap InvoiceNumber">sylvia@mail.ru</td>
+                              <td class="align-middle text-end fs-0 white-space-nowrap OrderStatus"> <span class="badge badge rounded-pill badge-soft-warning">Processing<span class="ms-1 fas fa-stream" data-fa-transform="shrink-2"></span></span>
                               </td>
                           </tr>
                           <tr class="btn-reveal-trigger">
-                              <th class="align-middle white-space-nowrap name"><a href="../../app/e-commerce/customer-details.html">Edgar Allan Poe</a></th>
-                              <td class="align-middle white-space-nowrap email">edgar@yahoo.com</td>
-                              <td class="align-middle text-end fs-0 white-space-nowrap payment"> <span class="badge badge rounded-pill badge-soft-secondary">Blocked<span class="ms-1 fas fa-ban" data-fa-transform="shrink-2"></span></span>
+                              <th class="align-middle white-space-nowrap UniqueID"><a href="../../app/e-commerce/customer-details.html">Edgar Allan Poe</a></th>
+                              <td class="align-middle white-space-nowrap InvoiceNumber">edgar@yahoo.com</td>
+                              <td class="align-middle text-end fs-0 white-space-nowrap OrderStatus"> <span class="badge badge rounded-pill badge-soft-secondary">Upcoming New Order<span class="ms-1 fas fa-ban" data-fa-transform="shrink-2"></span></span>
                               </td>
                           </tr>
                           <tr class="btn-reveal-trigger">
-                              <th class="align-middle white-space-nowrap name"><a href="../../app/e-commerce/customer-details.html">William Butler Yeats</a></th>
-                              <td class="align-middle white-space-nowrap email">william@gmail.com</td>
-                              <td class="align-middle text-end fs-0 white-space-nowrap payment"> <span class="badge badge rounded-pill badge-soft-success">Success<span class="ms-1 fas fa-check" data-fa-transform="shrink-2"></span></span>
+                              <th class="align-middle white-space-nowrap UniqueID"><a href="../../app/e-commerce/customer-details.html">William Butler Yeats</a></th>
+                              <td class="align-middle white-space-nowrap InvoiceNumber">william@gmail.com</td>
+                              <td class="align-middle text-end fs-0 white-space-nowrap OrderStatus"> <span class="badge badge rounded-pill badge-soft-success">Completed Order<span class="ms-1 fas fa-check" data-fa-transform="shrink-2"></span></span>
                               </td>
                           </tr>
                           <tr class="btn-reveal-trigger">
-                              <th class="align-middle white-space-nowrap name"><a href="../../app/e-commerce/customer-details.html">Rabindranath Tagore</a></th>
-                              <td class="align-middle white-space-nowrap email">tagore@twitter.com</td>
-                              <td class="align-middle text-end fs-0 white-space-nowrap payment"> <span class="badge badge rounded-pill badge-soft-warning">Pending<span class="ms-1 fas fa-stream" data-fa-transform="shrink-2"></span></span>
+                              <th class="align-middle white-space-nowrap UniqueID"><a href="../../app/e-commerce/customer-details.html">Rabindranath Tagore</a></th>
+                              <td class="align-middle white-space-nowrap InvoiceNumber">tagore@twitter.com</td>
+                              <td class="align-middle text-end fs-0 white-space-nowrap OrderStatus"> <span class="badge badge rounded-pill badge-soft-warning">Processing<span class="ms-1 fas fa-stream" data-fa-transform="shrink-2"></span></span>
                               </td>
                           </tr>
                           <tr class="btn-reveal-trigger">
-                              <th class="align-middle white-space-nowrap name"><a href="../../app/e-commerce/customer-details.html">Emily Dickinson</a></th>
-                              <td class="align-middle white-space-nowrap email">emily@gmail.com</td>
-                              <td class="align-middle text-end fs-0 white-space-nowrap payment"> <span class="badge badge rounded-pill badge-soft-secondary">Blocked<span class="ms-1 fas fa-ban" data-fa-transform="shrink-2"></span></span>
+                              <th class="align-middle white-space-nowrap UniqueID"><a href="../../app/e-commerce/customer-details.html">Emily Dickinson</a></th>
+                              <td class="align-middle white-space-nowrap InvoiceNumber">emily@gmail.com</td>
+                              <td class="align-middle text-end fs-0 white-space-nowrap OrderStatus"> <span class="badge badge rounded-pill badge-soft-secondary">Upcoming New Order<span class="ms-1 fas fa-ban" data-fa-transform="shrink-2"></span></span>
                               </td>
                           </tr>
                           <tr class="btn-reveal-trigger">
-                              <th class="align-middle white-space-nowrap name"><a href="../../app/e-commerce/customer-details.html">Giovanni Boccaccio</a></th>
-                              <td class="align-middle white-space-nowrap email">giovanni@outlook.com</td>
-                              <td class="align-middle text-end fs-0 white-space-nowrap payment"> <span class="badge badge rounded-pill badge-soft-warning">Pending<span class="ms-1 fas fa-stream" data-fa-transform="shrink-2"></span></span>
+                              <th class="align-middle white-space-nowrap UniqueID"><a href="../../app/e-commerce/customer-details.html">Giovanni Boccaccio</a></th>
+                              <td class="align-middle white-space-nowrap InvoiceNumber">giovanni@outlook.com</td>
+                              <td class="align-middle text-end fs-0 white-space-nowrap OrderStatus"> <span class="badge badge rounded-pill badge-soft-warning">Processing<span class="ms-1 fas fa-stream" data-fa-transform="shrink-2"></span></span>
                               </td>
                           </tr>
                           <tr class="btn-reveal-trigger">
-                              <th class="align-middle white-space-nowrap name"><a href="../../app/e-commerce/customer-details.html">Oscar Wilde</a></th>
-                              <td class="align-middle white-space-nowrap email">oscar@hotmail.com</td>
-                              <td class="align-middle text-end fs-0 white-space-nowrap payment"> <span class="badge badge rounded-pill badge-soft-success">Success<span class="ms-1 fas fa-check" data-fa-transform="shrink-2"></span></span>
+                              <th class="align-middle white-space-nowrap UniqueID"><a href="../../app/e-commerce/customer-details.html">Oscar Wilde</a></th>
+                              <td class="align-middle white-space-nowrap InvoiceNumber">oscar@hotmail.com</td>
+                              <td class="align-middle text-end fs-0 white-space-nowrap OrderStatus"> <span class="badge badge rounded-pill badge-soft-success">Completed Order<span class="ms-1 fas fa-check" data-fa-transform="shrink-2"></span></span>
                               </td>
                           </tr>
                           <tr class="btn-reveal-trigger">
-                              <th class="align-middle white-space-nowrap name"><a href="../../app/e-commerce/customer-details.html">John Doe</a></th>
-                              <td class="align-middle white-space-nowrap email">doe@gmail.com</td>
-                              <td class="align-middle text-end fs-0 white-space-nowrap payment"> <span class="badge badge rounded-pill badge-soft-success">Success<span class="ms-1 fas fa-check" data-fa-transform="shrink-2"></span></span>
+                              <th class="align-middle white-space-nowrap UniqueID"><a href="../../app/e-commerce/customer-details.html">John Doe</a></th>
+                              <td class="align-middle white-space-nowrap InvoiceNumber">doe@gmail.com</td>
+                              <td class="align-middle text-end fs-0 white-space-nowrap OrderStatus"> <span class="badge badge rounded-pill badge-soft-success">Completed Order<span class="ms-1 fas fa-check" data-fa-transform="shrink-2"></span></span>
                               </td>
                           </tr>
                           <tr class="btn-reveal-trigger">
-                              <th class="align-middle white-space-nowrap name"><a href="../../app/e-commerce/customer-details.html">Emma Watson</a></th>
-                              <td class="align-middle white-space-nowrap email">emma@gmail.com</td>
-                              <td class="align-middle text-end fs-0 white-space-nowrap payment"> <span class="badge badge rounded-pill badge-soft-warning">Pending<span class="ms-1 fas fa-stream" data-fa-transform="shrink-2"></span></span>
+                              <th class="align-middle white-space-nowrap UniqueID"><a href="../../app/e-commerce/customer-details.html">Emma Watson</a></th>
+                              <td class="align-middle white-space-nowrap InvoiceNumber">emma@gmail.com</td>
+                              <td class="align-middle text-end fs-0 white-space-nowrap OrderStatus"> <span class="badge badge rounded-pill badge-soft-warning">Processing<span class="ms-1 fas fa-stream" data-fa-transform="shrink-2"></span></span>
                               </td>
                           </tr>
                           </tbody>
@@ -174,7 +176,7 @@ include "../config/danger_notification.php";
         </div>
       </div>
     </div>
-  </div>
+  </div> <!-- end of .container-->
 
   <div class="container mt-3">
     <div class="row">
